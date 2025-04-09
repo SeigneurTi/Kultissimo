2afc387efee3443fc25b4a370bb6f2f358bc083b
SeigneurTi <thomas.tilly@eemi.com>
2025-04-09 02:35:05 +0200

    Modify index.js

--- server/index.js
+++ server/index.js
@@ -1,1 +1,15 @@
-server.listen(PORT, '0.0.0.0', () => console.log(`Serveur en écoute sur le port ${PORT}`));
+const express = require("express");
+const http = require("http");
+const { Server } = require("socket.io");
+const path = require("path");
+const fs = require("fs");
+
+const app = express();
+const server = http.createServer(app); // <== C'EST CE QUI MANQUAIT
+const io = new Server(server);
+
+const PORT = process.env.PORT || 3000;
+
+server.listen(PORT, '0.0.0.0', () => {
+  console.log(`Serveur en écoute sur le port ${PORT}`);
+});
